988a5436026da0ee26da93f45fe12da94728cb92
ChristopherLau <christopherlaukl@gmail.com>
2019-05-16 10:56:46 +0800

    realtime plot

--- ibeacon_neuralnetwork/scripts/realtime_kalman_plot.py
+++ ibeacon_neuralnetwork/scripts/realtime_kalman_plot.py
@@ -1,40 +1,43 @@
+#!/usr/bin/python
+
 import time
 from beacontools import BeaconScanner, IBeaconFilter
 from kalman_filter_linear import KalmanFilterLinear
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-#rssi_value = 0
+#Kalman filter properties
 kalman =[]
-
 A = np.matrix([1])
 H = np.matrix([1])
 B = np.matrix([0])
 Q = np.matrix([0.00001])
 R = np.matrix([0.008])
-xhat = np.matrix([3])
+xhat = np.matrix([-80])
 P    = np.matrix([1])
 
+filter = KalmanFilterLinear(A,B,H,xhat,P,Q,R)
+
 def callback(bt_addr, rssi, packet, additional_info):
-    #print("<%s, %d>" % (additional_info, rssi))
-    print(additional_info)
-    #rssi_value = rssi
+    #print(additional_info)
+    kalman.append(filter.GetCurrentState()[0,0])
+    filter.Step(np.matrix([0]),np.matrix([rssi]))
+    print(kalman)
+    plt.plot(kalman)
+    plt.pause(0.1)
 
 def main():
+    
     while True:
-        # scan for all iBeacon advertisements from beacons with the specified uui
-        filter = KalmanFilterLinear(A,B,H,xhat,P,Q,R)
-        scanner = BeaconScanner(callback)
+       
+        # scan for all iBeacon advertisements from beacons with the specified uuid, major and minor
+        scanner = BeaconScanner(callback, device_filter=IBeaconFilter(uuid="b9407f30-f5f8-466e-aff9-25556b57fe6d", major=30174, minor=5667))
         scanner.start()
-        time.sleep(1)
-        #print(uuid)
-        #print(rssi_value)
-        print("\n")
+        time.sleep(20)
         scanner.stop()
 
-
-#kalman.append(filter.GetCurrentState()[0,0])
-#filter.Step(np.matrix([0]),np.matrix([rssi))
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -43,8 +46,3 @@ if __name__ == "__main__":
 
 
 
-
-
-
-#time.sleep(5)
-#scanner.stop()
